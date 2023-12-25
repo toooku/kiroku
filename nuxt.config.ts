3ed28dce908d87a00dc86a5cb3f4e6fabdac23f9
[toooku)] <[tokunagatukasa@gmail.com]>
2023-12-26 01:33:01 +0900

    init commit

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@element-plus/nuxt'
+  ],
+  elementPlus: { /** Options */ },
+  devtools: { enabled: true }
+})
